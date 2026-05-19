e4bfebddd8432c39bea82c1c1b1c392ef11c8192
soul-re123 <grizzlecian123@gmail.com>
2026-05-19 07:38:17 -0500

    Add files via upload

---
+++ galactic/galactic.js
@@ -0,0 +1,9 @@
+//Making an assistant for travel
+let shipName =prompt("What is the ships name?");
+let passengers =prompt("How many passengers are on board?");
+let speed =prompt("What is the speed of the ship in light years per hour?");
+let distance =prompt("What is the distance to your destination in light years?");
+
+let time = distance/speed;
+
+document.write("Sh")
